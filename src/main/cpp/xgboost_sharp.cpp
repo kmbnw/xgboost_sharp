62b8a16a814128381071bcae92b7ea4a59f39df7
Krysta Bouzek <nwkrystab@gmail.com>
2016-10-02 16:52:28 -0700

    Added license to top of source code file

--- src/main/cpp/xgboost_sharp.cpp
+++ src/main/cpp/xgboost_sharp.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Krysta M Bouzek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include "rabit/c_api.h"
@@ -70,7 +86,6 @@ void XGBoostWrapper::predict(const float Xs[], const float** Yhats, unsigned int
     DMatrixHandle h_test;
     XGDMatrixCreateFromMat((float *) Xs, rows, cols, -1, &h_test);
     bst_ulong out_len;
-    const float *f;
     XGBoosterPredict(m_booster, h_test, 0, 0, &out_len, Yhats);
 
     // free xgboost internal structures
