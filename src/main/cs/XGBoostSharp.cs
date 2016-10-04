03acc1daff9db28d713b3722255f504ac4f7275a
Krysta Bouzek <nwkrystab@gmail.com>
2016-10-03 21:51:13 -0700

    Added namespace

--- src/main/cs/XGBoostSharp.cs
+++ src/main/cs/XGBoostSharp.cs
@@ -18,86 +18,90 @@ using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class XGBoostAPI {
-    [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
-    [return: MarshalAs(UnmanagedType.I1)]
-    private static extern void DeleteBooster(IntPtr pXGBooster);
-
-    [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
-    [return: MarshalAs(UnmanagedType.I1)]
-    private static extern IntPtr CreateBooster(uint numTrees);
-
-    [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
-    [return: MarshalAs(UnmanagedType.I1)]
-    private static extern void Fit(
-        IntPtr pXGBooster,
-        float[] Xs,
-        float[] Ys,
-        uint rows,
-        uint cols);
-
-    [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
-    [return: MarshalAs(UnmanagedType.I1)]
-    private static extern void Predict(
-        IntPtr pXGBooster,
-        float[] Xs,
-        float[] Yhats,
-        uint rows,
-        uint cols);
-
-    public static void Main(string[] args)
+namespace XGBoostSharp
+{
+    public static class XGBoostAPI 
     {
-        IntPtr booster = IntPtr.Zero;
+        [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern void DeleteBooster(IntPtr pXGBooster);
 
-        const uint cols = 3;
-        const uint rows = 5;
+        [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern IntPtr CreateBooster(uint numTrees);
 
-        float[] Xs = new float[rows * cols];
-        for(uint i = 0; i < rows; ++i)
+        [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern void Fit(
+            IntPtr pXGBooster,
+            float[] Xs,
+            float[] Ys,
+            uint rows,
+            uint cols);
+
+        [DllImport("libxgboost_sharp.so", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern void Predict(
+            IntPtr pXGBooster,
+            float[] Xs,
+            float[] Yhats,
+            uint rows,
+            uint cols);
+
+        public static void Main(string[] args)
         {
-            for(uint j = 0; j < cols; ++j)
+            IntPtr booster = IntPtr.Zero;
+
+            const uint cols = 3;
+            const uint rows = 5;
+
+            float[] Xs = new float[rows * cols];
+            for(uint i = 0; i < rows; ++i)
             {
-                Xs[i * cols + j] = (i + 1) * (j + 1);
+                for(uint j = 0; j < cols; ++j)
+                {
+                    Xs[i * cols + j] = (i + 1) * (j + 1);
+                }
             }
-        }
 
-        float[] Ys = new float[rows];
+            float[] Ys = new float[rows];
 
-        for (uint i = 0; i < rows; i++) 
-        {
-            Ys[i] = 1 + i * i * i;
-        }
-
-        const int sample_rows = 5;
-        float[] test = new float[sample_rows * cols];
-        for (int i = 0;i < sample_rows; i++) 
-        {
-            for (uint j = 0; j < cols; j++) 
+            for (uint i = 0; i < rows; i++) 
             {
-                test[i * cols + j] = (i + 1) * (j + 1);
+                Ys[i] = 1 + i * i * i;
             }
-        }
 
-        float[] Yhats = new float[sample_rows];
-        
-        try 
-        {
-            booster = CreateBooster(200);
-            Fit(booster, Xs, Ys, rows, cols);
-
-            // XGBoost allocates the array for us, so just grab the pointer
-            //IntPtr yhatsPtr = IntPtr.Zero;
-            Predict(booster, test, Yhats, sample_rows, cols);
-            //Marshal.Copy(yhatsPtr, Yhats, 0, sample_rows);
-            //Marshal.Free(yhatsPtr);
-
-            for (uint i = 0;i < sample_rows; i++) {
-                Console.WriteLine("prediction[" + i + "]=" + Yhats[i]);
+            const int sample_rows = 5;
+            float[] test = new float[sample_rows * cols];
+            for (int i = 0;i < sample_rows; i++) 
+            {
+                for (uint j = 0; j < cols; j++) 
+                {
+                    test[i * cols + j] = (i + 1) * (j + 1);
+                }
             }
-        }
-        finally
-        {
-            DeleteBooster(booster);
+
+            float[] Yhats = new float[sample_rows];
+            
+            try 
+            {
+                booster = CreateBooster(200);
+                Fit(booster, Xs, Ys, rows, cols);
+
+                // XGBoost allocates the array for us, so just grab the pointer
+                //IntPtr yhatsPtr = IntPtr.Zero;
+                Predict(booster, test, Yhats, sample_rows, cols);
+                //Marshal.Copy(yhatsPtr, Yhats, 0, sample_rows);
+                //Marshal.Free(yhatsPtr);
+
+                for (uint i = 0;i < sample_rows; i++) {
+                    Console.WriteLine("prediction[" + i + "]=" + Yhats[i]);
+                }
+            }
+            finally
+            {
+                DeleteBooster(booster);
+            }
         }
     }
 }
